4f3f06f374a4d0f96f5c859f7b0c6fce6c6a9c9b
hmalik88 <hbmalik88@gmail.com>
2021-10-07 21:26:29 -0400

    added chai, added registry contract and tests

---
+++ contracts/Registry.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ *@title Morphware Registry: Holds contract, auction and version information
+ *@notice contains methods to set contract addresses, auction durations and client versions
+ */
+
+contract Registry is Ownable {
+
+    address public auctionContract;
+    string public clientVersion;
+    address public jobContract;
+    uint public biddingDuration;
+    uint public revealDuration;
+
+
+  /**
+   * @notice sets auction contract address
+   * @param _auctionAddress new auction contract address
+   */
+
+    function setAuctionContract(address _auctionAddress) public onlyOwner {
+        auctionContract = _auctionAddress;
+    }
+
+  /**
+   * @notice sets job contract address
+   * @param _jobAddress new job contract address
+   */
+
+    function setJobContract(address _jobAddress) public onlyOwner {
+        jobContract = _jobAddress;
+    }
+
+  /**
+   * @notice sets client version
+   * @param _clientVersion new client version
+   */
+
+    function setClientVersion(string memory _clientVersion) public onlyOwner {
+        clientVersion = _clientVersion;
+    }
+
+  /**
+   * @notice sets bidding period duration
+   * @param _biddingDuration new bidding duration
+   */
+
+    function setBiddingDuration(uint _biddingDuration) public onlyOwner {
+        biddingDuration = _biddingDuration;
+    }
+
+  /**
+   * @notice sets reveal period duration
+   * @param _revealDuration new reveal duration
+   */
+
+    function setRevealDuration(uint _revealDuration) public onlyOwner {
+        revealDuration = _revealDuration;
+    }
+    
+}
