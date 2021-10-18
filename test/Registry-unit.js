7fe13a5d733ec0d4cdb2b2374e94c8abe14196fb
hmalik88 <hbmalik88@gmail.com>
2021-10-18 19:40:19 -0400

    added constructor test

--- test/Registry-unit.js
+++ test/Registry-unit.js
@@ -7,31 +7,45 @@ describe("Registry", () => {
   before(async () => {
     [deployer, addr1, addr2] = await ethers.getSigners();
     const RegistryFactory = await ethers.getContractFactory("Registry");
-    registry = await RegistryFactory.deploy();
+    registry = await RegistryFactory.deploy(
+      addr1.address,
+      "0.1.0",
+      addr2.address,
+      120,
+      120
+    );
     await registry.deployed();
   });
-  it("should be able to set the auction contract address", async () => {
-    await registry.setAuctionContract(addr1.address);
+
+  it("should set the state variables on deployment", async () => {
     expect(await registry.auctionContract()).to.equal(addr1.address);
+    expect(await registry.jobContract()).to.equal(addr2.address);
+    expect(await registry.clientVersion()).to.equal("0.1.0");
+    expect(await registry.biddingDuration()).to.equal(120);
+    expect(await registry.revealDuration()).to.equal(120);
+  });
+  it("should be able to set the auction contract address", async () => {
+    await registry.setAuctionContract(addr2.address);
+    expect(await registry.auctionContract()).to.equal(addr2.address);
   });
 
   it("should be able to set the jobFactory contract address", async () => {
-    await registry.setJobContract(addr2.address);
-    expect(await registry.jobContract()).to.equal(addr2.address);
+    await registry.setJobContract(addr1.address);
+    expect(await registry.jobContract()).to.equal(addr1.address);
   });
 
   it("should be able to set the client version", async () => {
-    await registry.setClientVersion("0.1.0");
-    expect(await registry.clientVersion()).to.equal("0.1.0");
+    await registry.setClientVersion("0.2.0");
+    expect(await registry.clientVersion()).to.equal("0.2.0");
   });
 
   it("should be able to set the bidding duration", async () => {
-    await registry.setBiddingDuration(120);
-    expect(await registry.biddingDuration()).to.equal(120);
+    await registry.setBiddingDuration(140);
+    expect(await registry.biddingDuration()).to.equal(140);
   });
 
   it("should be able to set the reveal duration", async () => {
-    await registry.setRevealDuration(120);
-    expect(await registry.revealDuration()).to.equal(120);
+    await registry.setRevealDuration(200);
+    expect(await registry.revealDuration()).to.equal(200);
   });
 });
