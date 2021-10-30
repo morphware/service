12663c8b613e5542fc4ea73481ddaef17d3a78a7
hmalik88 <hbmalik88@gmail.com>
2021-10-30 18:29:03 -0400

    updated deploys

--- deploys.js
+++ deploys.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+  registry: {
+    1: "",
+    4: "",
+  },
+  jobFactory: {
+    1: "",
+    4: "",
+  },
+  morphwareToken: {
+    1: "",
+    4: "",
+  },
+  auctionFactory: {
+    1: "",
+    4: "",
+  },
+};
